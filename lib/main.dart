9d3f280eb83d82822db6abf2ba999d7705efa545
Victor-Valedev <victorvaledev@gmail.com>
2025-04-10 11:58:18 -0300

    feat: Cria modal para adicionar despesas

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,96 @@
+import 'package:expensivesapp/components/transactions_forms.dart';
+import 'package:expensivesapp/models/transactions.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
-
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  
+  final _transactions = [
+    Transactions(
+      id: 't1',
+      title: 'Tênis Nike AirForce',
+      date: DateTime.now(),
+      value: 220.85,
+    ),
+    Transactions(
+      id: 't2',
+      title: 'Macbook M1',
+      date: DateTime.now(),
+      value: 8500.85,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Center(
-        child: Text('Versão incial'),
+      appBar: AppBar(title: Text('Despesas Pessoais')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.lightBlue,
+              child: Text('Gráfico Widget'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children:
+                _transactions.map((tr) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.orange, width: 2),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'R\$ ${tr.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.orange,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tr.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('d MMM y').format(tr.date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+          ),
+          TransactionsForms()
+        ],
       ),
     );
   }
