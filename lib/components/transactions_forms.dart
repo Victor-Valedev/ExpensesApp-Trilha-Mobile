bde6adc33acd4caa32a99a127a1e929bd05958c6
Victor-Valedev <victorvaledev@gmail.com>
2025-04-14 14:47:19 -0300

    feat: Cria o calendário para selecionar a data da transação (ainda sem ação)

--- lib/components/transactions_forms.dart
+++ lib/components/transactions_forms.dart
@@ -10,18 +10,27 @@ class TransactionsForms extends StatefulWidget {
 }
 
 class _TransactionsFormsState extends State<TransactionsForms> {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
 
   _submitForms() {
-    final title = titleController.text;
-    final value = double.tryParse(valueController.text) ?? 0.0;
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ?? 0.0;
 
     if (title.isEmpty || value <= 0) {
       return;
     }
-    
+
     widget.onSubmit(title, value);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime.now(),
+    );
   }
 
   @override
@@ -32,20 +41,37 @@ class _TransactionsFormsState extends State<TransactionsForms> {
         child: Column(
           children: <Widget>[
             TextField(
-              controller: titleController,
+              controller: _titleController,
               onSubmitted: (_) => _submitForms(),
               decoration: InputDecoration(labelText: 'Título'),
             ),
             TextField(
-              controller: valueController,
+              controller: _valueController,
               onSubmitted: (_) => _submitForms(),
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               decoration: InputDecoration(labelText: 'Valor (R\$)'),
             ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('Nenhuma data selecionada!'),
+                  TextButton(
+                    child: Text('Selecionar data'),
+                    style: TextButton.styleFrom(
+                      foregroundColor: Theme.of(context).colorScheme.primary,
+                    ),
+                    onPressed: _showDatePicker,
+                  ),
+                ],
+              ),
+            ),
             SizedBox(height: 10),
             ElevatedButton(
               onPressed: _submitForms,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
               child: Text(
                 'Nova Transação',
                 style: TextStyle(
