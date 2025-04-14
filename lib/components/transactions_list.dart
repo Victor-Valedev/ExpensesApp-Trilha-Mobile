c78f2ac6e6d8e7cf7617b9120c1e11a7f5eece2c
Victor-Valedev <victorvaledev@gmail.com>
2025-04-14 16:34:14 -0300

    refactor: modifica o tamanho da lista dos cards

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -11,7 +11,7 @@ class TransactionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 560,
       child:
           transactions.isEmpty
               ? Column(
